5449786c5551541bf91b2f6ce49f4deb0bd4ca0a
Pierre Monvoisin <monvoisin.pierre@gmail.com>
2019-12-13 21:22:08 +0100

    Partie front en cours, formulaire et calendrier fonctinonel

---
+++ calendar.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang='fr' dir='ltr'>
+<head>
+  <title>Exercice 8</title>
+  <meta charset='UTF-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body class='container-fluid bg-secondary'>
+  <div class='row mt-5'>
+    <div class='jumbotron shadow-lg py-5 mx-auto text-center'>
+      <h2 class="w-100 text-center mb-4">- Calendrier -</h2>
+      <div id="calendar">
+        <div class="calendar">
+          <div class="base bottom">
+            <div class="days">
+              <p class="weekday">SUN</p>
+              <p class="weekday">MON</p>
+              <p class="weekday">TUE</p>
+              <p class="weekday">WED</p>
+              <p class="weekday">THU</p>
+              <p class="weekday">FRI</p>
+              <p class="weekday">SAT</p>
+              <p class="old">30</p>
+              <p>1</p>
+              <p>2</p>
+              <p>3</p>
+              <p>4</p>
+              <p>5</p>
+              <p>6</p>
+              <p>7</p>
+              <p>8</p>
+              <p>9</p>
+              <p>10</p>
+              <p>11</p>
+              <p>12</p>
+              <p>13</p>
+              <p>14</p>
+              <p>15</p>
+              <p>16</p>
+              <p>17</p>
+              <p>18</p>
+              <p>19</p>
+              <p>20</p>
+              <p>21</p>
+              <p>22</p>
+              <p>23</p>
+              <p>24</p>
+              <p>25</p>
+              <p>26</p>
+              <p>27</p>
+              <p>28</p>
+              <p>29</p>
+              <p>30</p>
+              <p>31</p>
+              <p class="old">1</p>
+              <p class="old">2</p>
+              <p class="old">3</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'></script>
+  <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang='fr' dir='ltr'>
+<head>
+  <title>Exercice 8</title>
+  <meta charset='UTF-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
+</head>
+
+<body class='container-fluid bg-secondary'>
+  <div class='row mt-5'>
+    <div class='jumbotron shadow-lg py-5 mx-auto text-center'>
+      <h2 class="w-100 text-center mb-4">Choisissez le mois et l'année de votre naissance :</h2>
+      <div id="calendar">
+        <form class="form-group mr-3" action="calendar.php" method="get">
+          <label for="month" class="font-weight-bold mb-3">Veuillez choisir un mois :</label>
+          <select name="month" class="form-control w-50 mx-auto mb-3" id="month">
+            <option name="month" disabled selected>-- Mois --</option>
+            <option name="month" value="January">Janvier</option>
+            <option name="month" value="February">Février</option>
+            <option name="month" value="March">Mars</option>
+            <option name="month" value="April">Avril</option>
+            <option name="month" value="May">Mai</option>
+            <option name="month" value="June">Juin</option>
+            <option name="month" value="July">Juillet</option>
+            <option name="month" value="August">Août</option>
+            <option name="month" value="September">Septembre</option>
+            <option name="month" value="October">Octobre</option>
+            <option name="month" value="November">Novembre</option>
+            <option name="month" value="December">Décembre</option>
+          </select>
+          <label for="year" class="font-weight-bold mb-3">Veuillez choisir une année :</label>
+          <select name="year" class="form-control w-50 mx-auto mb-3" id="year">
+            <option name="year" disabled selected>-- Année --</option>
+            <?php
+            foreach (range(date('Y'), 1500) as $year) { ?>
+              <option name="year" value="<?= $year ?>"><?= $year ?></option>
+            <?php } ?>
+          </select>
+          <button type="submit" class="btn btn-dark">Valider</button>
+        </form>
+      </div>
+    </div>
+  </div>
+  <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'></script>
+  <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'></script>
+</body>
+</html>
